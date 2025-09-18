02f54190ab89a9de21977ab5e0e83b08a2e5bad5
Jose Ojeda <joche.ojeda@bitframeworks.com>
2025-09-18 16:31:26 -0600

    first commit

---
+++ XafKeycloakAuth.Blazor.Server/BlazorApplication.cs
@@ -0,0 +1,53 @@
+﻿using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.ApplicationBuilder;
+using DevExpress.ExpressApp.Blazor;
+using DevExpress.ExpressApp.Updating;
+using DevExpress.ExpressApp.Security;
+using DevExpress.ExpressApp.Security.ClientServer;
+using DevExpress.ExpressApp.SystemModule;
+using XafKeycloakAuth.Module.BusinessObjects;
+using Microsoft.EntityFrameworkCore;
+using DevExpress.ExpressApp.EFCore;
+using DevExpress.EntityFrameworkCore.Security;
+
+namespace XafKeycloakAuth.Blazor.Server;
+
+public class XafKeycloakAuthBlazorApplication : BlazorApplication {
+    public XafKeycloakAuthBlazorApplication() {
+        ApplicationName = "XafKeycloakAuth";
+        CheckCompatibilityType = DevExpress.ExpressApp.CheckCompatibilityType.DatabaseSchema;
+        DatabaseVersionMismatch += XafKeycloakAuthBlazorApplication_DatabaseVersionMismatch;
+    }
+    protected override void OnSetupStarted() {
+        base.OnSetupStarted();
+
+#if DEBUG
+        if(System.Diagnostics.Debugger.IsAttached && CheckCompatibilityType == CheckCompatibilityType.DatabaseSchema) {
+            DatabaseUpdateMode = DatabaseUpdateMode.UpdateDatabaseAlways;
+        }
+#endif
+    }
+    void XafKeycloakAuthBlazorApplication_DatabaseVersionMismatch(object sender, DatabaseVersionMismatchEventArgs e) {
+#if EASYTEST
+        e.Updater.Update();
+        e.Handled = true;
+#else
+        if(System.Diagnostics.Debugger.IsAttached) {
+            e.Updater.Update();
+            e.Handled = true;
+        }
+        else {
+            string message = "The application cannot connect to the specified database, " +
+                "because the database doesn't exist, its version is older " +
+                "than that of the application or its schema does not match " +
+                "the ORM data model structure. To avoid this error, use one " +
+                "of the solutions from the https://www.devexpress.com/kb=T367835 KB Article.";
+
+            if(e.CompatibilityError != null && e.CompatibilityError.Exception != null) {
+                message += "\r\n\r\nInner exception: " + e.CompatibilityError.Exception.Message;
+            }
+            throw new InvalidOperationException(message);
+        }
+#endif
+    }
+}
---
+++ XafKeycloakAuth.Module/BusinessObjects/XafKeycloakAuthDbContext.cs
@@ -0,0 +1,38 @@
+﻿using DevExpress.ExpressApp.EFCore.Updating;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using DevExpress.Persistent.BaseImpl.EF.PermissionPolicy;
+using DevExpress.Persistent.BaseImpl.EF;
+using DevExpress.ExpressApp.Design;
+using DevExpress.ExpressApp.EFCore.DesignTime;
+
+namespace XafKeycloakAuth.Module.BusinessObjects;
+
+
+[TypesInfoInitializer(typeof(DbContextTypesInfoInitializer<XafKeycloakAuthEFCoreDbContext>))]
+public class XafKeycloakAuthEFCoreDbContext : DbContext {
+    public XafKeycloakAuthEFCoreDbContext(DbContextOptions<XafKeycloakAuthEFCoreDbContext> options) : base(options) {
+    }
+    //public DbSet<ModuleInfo> ModulesInfo { get; set; }
+    public DbSet<ModelDifference> ModelDifferences { get; set; }
+    public DbSet<ModelDifferenceAspect> ModelDifferenceAspects { get; set; }
+    public DbSet<PermissionPolicyRole> Roles { get; set; }
+    public DbSet<XafKeycloakAuth.Module.BusinessObjects.ApplicationUser> Users { get; set; }
+    public DbSet<XafKeycloakAuth.Module.BusinessObjects.ApplicationUserLoginInfo> UserLoginsInfo { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder) {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.UseDeferredDeletion(this);
+        modelBuilder.UseOptimisticLock();
+        modelBuilder.SetOneToManyAssociationDeleteBehavior(DeleteBehavior.SetNull, DeleteBehavior.Cascade);
+        modelBuilder.HasChangeTrackingStrategy(ChangeTrackingStrategy.ChangingAndChangedNotificationsWithOriginalValues);
+        modelBuilder.UsePropertyAccessMode(PropertyAccessMode.PreferFieldDuringConstruction);
+        modelBuilder.Entity<XafKeycloakAuth.Module.BusinessObjects.ApplicationUserLoginInfo>(b => {
+            b.HasIndex(nameof(DevExpress.ExpressApp.Security.ISecurityUserLoginInfo.LoginProviderName), nameof(DevExpress.ExpressApp.Security.ISecurityUserLoginInfo.ProviderUserKey)).IsUnique();
+        });
+        modelBuilder.Entity<ModelDifference>()
+            .HasMany(t => t.Aspects)
+            .WithOne(t => t.Owner)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
